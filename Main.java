9b6c70851e60b751f1c6aee6310823d04833cd3e
BuntinWindows <aokitakumi72@gmail.com>
2022-08-10 08:04:18 +0900

    DiscordにWebhookでPOSTできるようになった。

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-import commands.Run;
+import src.DiscordWebhook;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Run cr = new Run();
-        cr.run();
+        DiscordWebhook wh = new DiscordWebhook("https://discord.com/api/webhooks/1006683750836486175/4CXxmCN4yKm6Elv8IoFMjAiqPNdfBToVnixYwJtyYp2uKdrT0qOlZ27YKmhKihiE8lfE");
+        wh.setContent("Hello World!");
+        wh.execute();
     }
 }
